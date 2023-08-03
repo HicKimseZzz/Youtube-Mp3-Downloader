1fb3f38ccec58a56726963c3f25b2727904bea62
Mrz <86775732+MrZ1922@users.noreply.github.com>
2023-08-03 22:10:01 +0300

    Update Mp3_Downloader.py
    Will Work On It

--- Mp3_Downloader.py
+++ Mp3_Downloader.py
@@ -9,11 +9,22 @@ import shutil
 dir1 = r""
 dir2 = r""
 
+def get_dir1():
+    dir1 = entry_dir1.get()
+    print(dir1)
+
+def get_dir2():
+    dir2 = entry_dir2.get()
+    print(dir2)
+
 def download():
+    link = entry_link.get()
     yt = YouTube(link)
     downYt = yt.title + ".mp4"
     yr = yt.streams.get_highest_resolution()
     print(yr)
+    print("---------------------------")
+    print(downYt)
 
     print("Your Video Is Downloading")
     yr.download()
@@ -24,9 +35,6 @@ def download():
             print("Finded")
             shutil.move(dir2 + f"\{file1}",dir1)
             break
-
-def close():
-    exit()
 
 def convert():
     for file in os.listdir(dir1):
@@ -47,13 +55,39 @@ linkStr = tk.StringVar()
 entry_link = Entry(master=frame_link,width=45,font=("Cascadia Code",10),textvariable=linkStr)
 linkBut = Button(master=frame_link,text="Download",command=download,font=("Cascadia Code",10),fg="#e8e8e8",bg="#222222",activebackground="#1D1D1D",activeforeground="#FAFBFB",relief=FLAT)
 
-mp3But = Button(master=window,text="Convert",font=("Cascadia Code",10),fg="#e8e8e8",bg="#222222",activebackground="#1D1D1D",activeforeground="#FAFBFB",relief=FLAT)
+frame_dir1 = Frame(master=window,bg="#242424")
+dir1Str = tk.StringVar()
+entry_dir1 = Entry(master=frame_dir1,width=45,font=("Cascadia Code",10),textvariable=dir1Str)
+dir1But = Button(master=frame_dir1,command=get_dir1,text="Submit",font=("Cascadia Code",10),fg="#e8e8e8",bg="#222222",activebackground="#1D1D1D",activeforeground="#FAFBFB",relief=FLAT)
 
-frame_link.pack(pady=100)
+frame_dir2 = Frame(master=window,bg="#242424")
+dir2Str = tk.StringVar()
+entry_dir2 = Entry(master=frame_dir2,width=45,font=("Cascadia Code",10),textvariable=dir2Str)
+dir2But = Button(master=frame_dir2,command=get_dir2,text="submit",font=("Cascadia Code",10),fg="#e8e8e8",bg="#222222",activebackground="#1D1D1D",activeforeground="#FAFBFB",relief=FLAT)
+
+mp3But = Button(master=window,command=convert,text="Convert",font=("Cascadia Code",10),fg="#e8e8e8",bg="#222222",activebackground="#1D1D1D",activeforeground="#FAFBFB",relief=FLAT)
+
+frame_dir2.pack(pady=30)
+entry_dir2.pack(side = "left",padx= 10)
+dir2But.pack(padx=10)
+dir2_text = Label(master=frame_dir2,text="Dir This File Is In",font=("Cascadia Code",10))
+dir2_text.pack(pady=5)
+
+frame_dir1.pack(pady=30)
+entry_dir1.pack(side = "left",padx= 10)
+dir1But.pack(padx=10)
+dir1_text = Label(master=frame_dir1,text="Dir Where Mp3s Go",font=("Cascadia Code",10))
+dir1_text.pack(pady=5)
+
+
+frame_link.pack(pady=30)
 entry_link.pack(side = "left",padx= 10)
 linkBut.pack(padx=10)
+link_text = Label(master=frame_link,text="Youtube Link",font=("Cascadia Code",10))
+link_text.pack(pady=5)
+
 mp3But.pack(pady=10)
 
-window.iconbitmap("app.ico")
+#window.iconbitmap("app.ico")
 
 window.mainloop()
